e0f88ef814664aae369c4f6fb384e8a6e1a10bf1
Youngeon Kim <ykim733@gmail.com>
2019-02-21 20:14:03 -0600

    conflict html

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+      console.log('The color is green.');
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,4 +11,4 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-});
+  });
