f3c31af0591fcb311ef41aff628bddc5e2e59c2c
Brian Liang <BrianLiang2020@u.northwestern.edu>
2019-03-06 13:50:10 -0600

    goals in background updated

---
+++ background.js
@@ -0,0 +1,17 @@
+/*
+Make sure:
+1. All Headers are white
+2. All Links are lightblue
+3. Video and Audio tags show up
+4. a Tags for images show up and are proportional
+    b Tags for images have on hover descriptions
+    c Alt text for images is pink
+5. Input tags show up
+
+Make sure all background is black
+Make sure no styles are messed up
+Make sure headers / info persists
+
+Tabs for popup info
+Nicer button / separate button for black out?
+*/
