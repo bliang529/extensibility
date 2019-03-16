06c111d6a381dca18ecdd726a19d36d7013adbc7
Brian Liang <Brian@Brians-Mac-Pro.local>
2019-03-15 20:44:41 -0500

    new icon images

--- background.js
+++ background.js
@@ -3,21 +3,9 @@ Headers - white, Links - lightblue, Alt text - pink
 
 Priority:
 
-1. Video, Audio, Input tags show up + look decent
-
-
-Nice to have:
-
-1. Make sure all background is black
-2. Nicer popup design
-3. Nicer buttons
-
-
-
-Pie:
-
-1. Info is mapped on html
-2. Better icons
-3. Tags for images have on hover descriptions
+1. Key/Tags for Headers/Links + Input tags show up + look decent
+2. Make sure all background is black
+3. Nicer popup design
+4. Tags for images have on hover descriptions
 
 */
