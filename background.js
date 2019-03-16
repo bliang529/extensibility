cf52b1a4dac1c497dc67e1e45369f0e2e2342429
Brian Liang <Brian@Brians-Mac-Pro.local>
2019-03-15 22:02:36 -0500

    nicer popup

--- background.js
+++ background.js
@@ -4,8 +4,8 @@ Headers - white, Links - lightblue, Alt text - pink
 Priority:
 
 1. Key/Tags for Headers/Links + Input tags show up + look decent
+
 2. Make sure all background is black
-3. Nicer popup design
-4. Tags for images have on hover descriptions
+3. Tags for images have on hover descriptions
 
 */
